f245e2277891c9768c1faee437f17b6d78b032e9
hakusingo <hakusingo@gmail.com>
2023-02-24 21:22:36 +0900

    fix

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,6 @@ import { Link } from "gatsby"
 import FooterLogo from "./svg/footer-logo"
 import "./footer.scss"
 import FooterLogoPc from "./svg/footer-logo-pc"
-// import footerPcLogo from "../images/svg/footer/footer-pc-logo.svg"
 
 const footerSpLink = [
   {
@@ -102,10 +101,9 @@ const Footer = () => (
             to={`/`}
           >
             <FooterLogoPc/>
-            {/* <img src={footerPcLogo} alt="" /> */}
           </Link>
           <div className="">
-            <h3 className="pt-4 text-main font-semibold">
+            <h3 className="pt-4 text-main-blue font-bold">
               沖縄自然体験アドベンチャーツアー
               ワンオーシャン
             </h3>
