ed304689d72f7c7a0c7a7edcf9a41fe187c86694
hakusingo <hakusingo@gmail.com>
2023-02-25 10:55:10 +0900

    Fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,12 +28,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-lodash`,
-      options: {
-        disabledFeatures: [`shorthands`, `cloning`],
-      },
-    },
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
